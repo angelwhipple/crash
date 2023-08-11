30cd09b54979c6f9b469a57f4d4a8c8fcca65f2a
Angel Whipple <awhipp@mit.edu>
2023-08-11 11:34:33 -0700

    navbar updates

--- client/src/components/modules/NavBar.tsx
+++ client/src/components/modules/NavBar.tsx
@@ -5,7 +5,10 @@ import { socket } from "../../client-socket";
 import { io } from "socket.io-client";
 import { get, post } from "../../utilities";
 import { RouteComponentProps, useNavigate } from "@reach/router";
-import { set } from "mongoose";
+import { HiHome } from "react-icons/hi";
+import { BsSearch, BsPersonFill } from "react-icons/bs";
+import { IoIosPeople } from "react-icons/io";
+import { query } from "express";
 
 type Props = RouteComponentProps & {};
 
@@ -13,6 +16,7 @@ const NavBar = (props) => {
   const [profile, setProfile] = useState(false);
   const [communities, setCommunities] = useState(false);
   const [housing, setHousing] = useState(false);
+  const [querying, setQuerying] = useState(false);
 
   const toggleTabs = (selectedFunc: (val: boolean) => void, all: boolean = false) => {
     const funcOptions = [setProfile, setCommunities, setHousing];
@@ -41,34 +45,37 @@ const NavBar = (props) => {
 
   return (
     <nav className="nav-bar-container">
-      <button
-        className={`u-pointer ${profile ? "nav-button-selected" : "nav-button"}`}
+      <BsPersonFill
+        className={`u-pointer ${profile ? "nav-icon-selected" : "nav-icon"}`}
         onClick={(event) => {
           route("/profile");
           toggleTabs(setProfile);
         }}
-      >
-        profile
-      </button>
-      <button
-        className={`u-pointer ${communities ? "nav-button-selected" : "nav-button"}`}
-        onClick={(event) => {
-          route("/communities");
-          toggleTabs(setCommunities);
-        }}
-      >
-        communities
-      </button>
-      <button
-        className={`u-pointer ${housing ? "nav-button-selected" : "nav-button"}`}
+      ></BsPersonFill>
+      <HiHome
+        className={`u-pointer ${housing ? "nav-icon-selected" : "nav-icon"}`}
         onClick={(event) => {
           route("/housing");
           toggleTabs(setHousing);
         }}
-      >
-        housing
-      </button>
-      <button className="nav-button u-pointer">search</button>
+      ></HiHome>
+      <IoIosPeople
+        className={`u-pointer ${communities ? "nav-icon-selected" : "nav-icon"}`}
+        onClick={(event) => {
+          route("/communities");
+          toggleTabs(setCommunities);
+        }}
+      ></IoIosPeople>
+      <BsSearch
+        className="nav-icon u-pointer"
+        onClick={(event) => {
+          setQuerying(!querying);
+        }}
+      ></BsSearch>
+      <input
+        type="search"
+        className={`${querying ? "search-bar-open" : "search-bar-close"}`}
+      ></input>
       <></>
     </nav>
   );
